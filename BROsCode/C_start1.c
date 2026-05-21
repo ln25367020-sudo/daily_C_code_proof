f9b8277a52c529458e94fb90d6fb71d03929ea07
NyanLinHtet <ln25367020@ga.ttc.ac.jp>
2026-05-22 02:11:40 +0900

    Day 2 basic C practice

---
+++ BROsCode/C_start1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    //Relearning
+
+    /*
+    This is for the passage comment
+    */
+    printf("I love Pizza\n");
+    printf("What about you?");
+    return 0;
+}
---
+++ C_start2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int x = 1;
+    int y= 2;
+
+    int total = x + y;
+    printf("The total is %d",total);
+    return 0;
+}
