c24c76f18aa82029f10b3931f9118b9b34923416
NyanLinHtet <ln25367020@ga.ttc.ac.jp>
2026-05-23 01:20:07 +0900

    This time I added scanf and learn how to use git

--- C_start2.c
+++ C_start2.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    int x = 1;
-    int y= 2;
+    int x;
+    int y;
+    printf("Enter x:");
+    scanf("%d",&x);
 
-    int total = x + y;
-    printf("The total is %d",total);
+    printf("Enter y:");
+    scanf("%d", &y);
+
+
+    int add = x + y;
+    int sub = x - y;
+    int multi = x * y;
+    printf("The total is %d \n",add);
+    printf("Substract is %d \n", sub );
+    printf("Multiply is %d\n",multi);
+
+    if(y !=0){
+         double div = (double)x/y;
+         printf("Division is %lf \n",div );
+    }else{
+        printf("Can not be divided by 0!!");
+    }
+
+
+
     return 0;
 }
